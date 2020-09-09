8cbf90139a351169d263fd9276cf6879e86ba0e0
dtuckersuh <suh977@gmail.com>
2020-09-08 23:30:07 -0700

    part1: add ComponentState notes and exercises

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,64 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
+const Header = ({ text }) => {
     return (
         <div>
-            <h1>{props.name}</h1>
+            <h1>{ text }</h1>
         </div>
     )
 }
-const Content = (props) => {
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+const Statistics = (props) => {
+    let all = props.good + props.neutral + props.bad
+    let positive = (props.good / all) * 100
+    if (all === 0) {
+        return (
+            <div>
+                <p>No feedback given</p>
+            </div>   
+        )
+    }
     return (
         <div>
-        {props.parts.map(part => {
-            return (<Part name={part.name} exercise={part.exercises} />)
-        })}
+            <p>good: {props.good}</p>
+            <p>neutral: {props.neutral}</p>
+            <p>bad: {props.bad}</p>
+            <p>all: {all}</p>
+            <p>average: {(props.good - props.bad) / all}</p>
+            <p>positive: {positive}%</p>
         </div>
     )
 }
-const Part = (props) => {
-    return (
-        <div>
-            <p>{props.name} {props.exercise}</p>
-        </div>
-    )
-}
-const Total = (props) => {
-    let sum = 0
-    props.parts.forEach(part => {
-        sum += part.exercises
-    })
-    return (
-        <div>
-            <p>Number of exercises {sum}</p>
-        </div>
-    )
-}
+
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts : [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    } 
-    
+    // Save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
     return (
         <div>
-            <Header name={course.name}/>
-            <Content parts={course.parts}/>
-            <Total parts={course.parts}/> 
+            <Header text="Give Feedback"/>
+            <Button onClick={handleGoodClick} text="good"/>
+            <Button onClick={handleNeutralClick} text="neutral"/>
+            <Button onClick={handleBadClick} text="bad"/>
+            <Header text="Statistics" />
+            <Statistics good={good} neutral={neutral} bad={bad} /> 
         </div>
     )
 }
