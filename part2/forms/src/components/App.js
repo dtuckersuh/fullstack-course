094c85a340a8b196a19f34332daa0f9ec01ea5da
dtuckersuh <suh977@gmail.com>
2020-09-12 23:49:34 -0700

    part2 forms notes complete exercise start

---
+++ part2/forms/src/components/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const App = () => {
+    const [ persons, setPersons ] = useState([
+        { name: 'Arto Hellas', key: 'Hellas'}
+    ])
+    const [ newName, setNewName ] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        if (persons.some((person) => person.name === newName)){
+            window.alert(`${newName} already added!`)
+        } else {
+            const personObject = {
+                name: newName,
+                key: newName
+            }
+            setPersons(persons.concat(personObject)) 
+            setNewName('')
+        }
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {persons.map(person => <p>{person.name}</p>)}
+            </div>
+            <div>debug: { newName }</div>
+        </div>
+    )
+}
+export default App;
